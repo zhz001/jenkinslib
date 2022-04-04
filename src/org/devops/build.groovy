1ce453bd9d3fd9d8f406b64d5222c47fc7e08956
张张 <49469755+zhz001@users.noreply.github.com>
2022-04-04 13:27:50 +0800

    Update build.groovy

--- src/org/devops/build.groovy
+++ src/org/devops/build.groovy
@@ -3,12 +3,12 @@ package org.devops
 //构建类型
 def Build(buildType,buildShell){
     def buildTools = ["mvn":"M2","ant":"ANT","gradle":"GRADLE","npm":"NPM"]
-    
-  println("当前打印类型的为：${buildType}")
-  buildHome = tool buildTools[buildType]
+
+    println("当前打印类型的为：${buildType}")
+    buildHome = tool buildTools[buildType]
     if ("${buildType}" == "npm"){
         sh "export NODE_HOME=${npmHome} && export PATH=\$NODE_HOME/bin:\$PATH && ${npmHome}/bin/npm ${bulidShell}"
- 
+
     }
-  sh "${buildHome}/bin/${buildType} ${buildShell}"
+    sh "${buildHome}/bin/${buildType} ${buildShell}"
 }
