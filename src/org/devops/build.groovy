68c2b853061f8f995784e9b9cf9dee2c3415feae
张张 <49469755+zhz001@users.noreply.github.com>
2022-04-04 13:23:19 +0800

    Update build.groovy

--- src/org/devops/build.groovy
+++ src/org/devops/build.groovy
@@ -6,6 +6,9 @@ def Build(buildType,buildShell){
     
   println("当前打印类型的为：${buildType}")
   buildHome = tool buildTools[buildType]
-  
+    if ("${buildType}" == "npm"){
+        sh "export NODE_HOME=${npmHome} && export PATH=\$NODE_HOME/bin:\$PATH && ${npmHome}/bin/npm ${bulidShell}"
+ 
+    }
   sh "${buildHome}/bin/${buildType} ${buildShell}"
 }
