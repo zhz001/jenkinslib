c857acbf5e2661899d4acb0907f10145d10c6e0c
张张 <49469755+zhz001@users.noreply.github.com>
2022-04-04 13:37:38 +0800

    Update build.groovy

--- src/org/devops/build.groovy
+++ src/org/devops/build.groovy
@@ -1,14 +1,35 @@
 package org.devops
 
+
 //构建类型
 def Build(buildType,buildShell){
     def buildTools = ["mvn":"M2","ant":"ANT","gradle":"GRADLE","npm":"NPM"]
-
-    println("当前打印类型的为：${buildType}")
-    buildHome = tool buildTools[buildType]
+    
+    
+    println("当前选择的构建类型为 ${buildType}")
+    buildHome= tool buildTools[buildType]
+    
     if ("${buildType}" == "npm"){
-        sh "export NODE_HOME=${npmHome} && export PATH=\$NODE_HOME/bin:\$PATH && ${npmHome}/bin/npm ${bulidShell}"
-
+        
+        sh  """ 
+            export NODE_HOME=${buildHome} 
+            export PATH=\$NODE_HOME/bin:\$PATH 
+            ${buildHome}/bin/${buildType} ${buildShell}"""
+    } else {
+        sh "${buildHome}/bin/${buildType}  ${buildShell}"
     }
-    sh "${buildHome}/bin/${buildType} ${buildShell}"
 }
+// package org.devops
+
+// //构建类型
+// def Build(buildType,buildShell){
+//     def buildTools = ["mvn":"M2","ant":"ANT","gradle":"GRADLE","npm":"NPM"]
+
+//     println("当前打印类型的为：${buildType}")
+//     buildHome = tool buildTools[buildType]
+//     if ("${buildType}" == "npm"){
+//         sh "export NODE_HOME=${npmHome} && export PATH=\$NODE_HOME/bin:\$PATH && ${npmHome}/bin/npm ${bulidShell}"
+
+//     }
+//     sh "${buildHome}/bin/${buildType} ${buildShell}"
+// }
